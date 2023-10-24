b0a56f4bd8f8ab912e7a194bb347754593b60270
Sandy Sa <sandysa0727@gmail.com>
2023-10-25 07:40:18 +1100

    modified path for mac

--- reports/plots/mimiciii_code_dist.py
+++ reports/plots/mimiciii_code_dist.py
@@ -6,15 +6,14 @@ import pandas as pd
 # Plot showing the distribution of ICD-9 and ICD-10 codes in MIMIC-IV
 
 
-mimiciv_hosp = pd.read_csv(
-    "/Users/full_of_bugs/data/mimic-data/mimic-iv-2.2/hosp/diagnoses_icd.csv.gz",
+mimiciii_diagnoses = pd.read_csv(
+    "/Users/full_of_bugs/data/mimic-data/mimic-iii-clinical-database-1.4/DIAGNOSES_ICD.csv.gz",
     compression="gzip",
 )
 
 sns.set_theme("paper", style="whitegrid", palette="colorblind", font_scale=1.5)
 
-icd9_counts = mimiciv_hosp[mimiciv_hosp.icd_version == 9].icd_code.value_counts()
-icd10_counts = mimiciv_hosp[mimiciv_hosp.icd_version == 10].icd_code.value_counts()
+icd9_counts = mimiciii_diagnoses.ICD9_CODE.value_counts()
 
 fig = plt.figure(figsize=(10, 5))
 ax = sns.lineplot(
@@ -23,22 +22,15 @@ ax = sns.lineplot(
     label="ICD-9",
     linewidth=1.5,
 )
-sns.lineplot(
-    x=np.linspace(0, len(icd10_counts) - 1, len(icd10_counts)),
-    y=icd10_counts.values,
-    label="ICD-10",
-    ax=ax,
-    linewidth=1.5,
-)
 ax.set(
     yscale="log",
     xlabel="Code index",
-    ylabel="Frequency of code in MIMIC-IV",
+    ylabel="Frequency of code in MIMIC-III",
 )
 ax.hlines(10, 0, 16200, linestyles="dashed", label="100", color="gray")
 plt.xlim(-50, 16200)
 fig.savefig(
-    "files/images/mimiciv_code_freq.png",
+    "files/images/mimiciii_code_freq.png",
     dpi=600,
     bbox_inches="tight",
 )
