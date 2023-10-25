1b4c1b139e9854ff0e877b3c93a372550064376d
Sandy-Sa <sandysa0727@gmail.com>
2023-10-25 07:16:05 +0000

    remove code irralevant to assignment

--- run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-source /home/sandy/miniconda3/bin/activate coding
-
-for chunk in 128 512; do
-    python main.py experiment=mimiciv_icd10/plm_icd gpu=1 dataset.configs.chunk_size=$chunk
-done
